1ce0ed704c238826bffc5be0a16a4ab5ed304e67
思甯 王 <107111214@mail.oit.edu.tw>
2021-03-23 15:06:55 +0800

    123

---
+++ 1HW/1HW.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int num = 12;
+            for (int i =2;i<num;i++)
+            {
+                if(num %i == 0)
+                
+            }
+        }
+    }
+}
